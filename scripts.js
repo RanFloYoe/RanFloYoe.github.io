416a3c20191d703c2a91343d14d0d278dd7525af
RanFloYoe <Proxygod19999@gmx.com>
2025-05-14 10:27:48 +0800

    v2

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    // 平滑滚动
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click',
